b874f9e37dadb24df0adf210c82ba65fb8be3127
Alex Rusin <Rusin.AI@phystech.edu>
2020-10-19 16:33:03 +0300

    Update tech04-2.c

--- tech-04/tech04-2.c
+++ tech-04/tech04-2.c
@@ -1,57 +1,66 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <limits.h>
-#include <stdlib.h>
 #include <fcntl.h>
-#include <string.h>
 #include <libgen.h>
-#include <sys/mman.h>
+#include <limits.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 enum ERRORS {
-    OK              = 0,
-    UNKNOWN_ERROR   = 1,
+    OK = 0,
+    UNKNOWN_ERROR = 1,
 };
 
-void fill_table(int* buff, int n, int w) {
+void printer(char* buff, int pos, int w, int cx)
+{
+    char tmp_c = *(buff + pos + w);
+    snprintf(buff + pos, w + 1, "%*d", w, cx);
+    *(buff + pos + w) = tmp_c;
+}
 
-    int i = 0, j = 0, k = 0, cx = 1;
- 
+void fill_table(char* buff, int n, int w)
+{
+    int i = 0, k = 0, cx = 1, pos = 0;
+
     while (i < n * n) {
-        
         k++;
 
-        for (j = k - 1; j < n - k + 1; j++) {
-            // sprintf(buff + ((k - 1) * n + j) * w, "%*d", w, cx++);
-            buff[(k - 1) * n + j] = cx++;
+        for (int j = k - 1; j < n - k + 1; j++) {
+            pos = ((k - 1) * n + j) * w + (k - 1);
+            printer(buff, pos, w, cx++);
             i++;
         }
 
-        for (j = k; j < n - k + 1; j++) {
-            // sprintf(buff + (j * n + n - k) * w, "%*d", w, cx++);
-            buff[j * n + n - k] = cx++;
-            i++;
-        }
- 
-        for (j = n - k - 1; j >= k - 1; j--) {
-            // sprintf(buff + ((n - k) * n + j) * w, "%*d", w, cx++);
-            buff[(n - k) * n + j] = cx++;
+        for (int j = k; j < n - k + 1; j++) {
+            pos = (j * n + n - k) * w + (j);
+            printer(buff, pos, w, cx++);
             i++;
         }
 
-        for (j = n - k - 1; j >= k; j--) {
-            // sprintf(buff + (j * n + k - 1) * w, "%*d", w, cx++);
-            buff[j * n + k - 1] = cx++;
+        for (int j = n - k - 1; j >= k - 1; j--) {
+            pos = ((n - k) * n + j) * w + (n - k);
+            printer(buff, pos, w, cx++);
+            i++;
+        }
+
+        for (int j = n - k - 1; j >= k; j--) {
+            pos = (j * n + k - 1) * w + (j);
+            printer(buff, pos, w, cx++);
             i++;
         }
     }
-    
+
+    for (i = 1; i <= n; ++i) {
+        *(buff + i * w * n + i - 1) = '\n';
+    }
 }
 
-int main(int argc, const char* argv[]) {
-
+int main(int argc, const char* argv[])
+{
     int n = strtol(argv[2], NULL, 10);
     int w = strtol(argv[3], NULL, 10);
 
@@ -60,40 +69,25 @@ int main(int argc, const char* argv[]) {
         return UNKNOWN_ERROR;
     }
 
-    size_t file_size = n * n * w + n; // n * n - elems, each takes w bytes + n bytes for all '\n'
+    // n * n - elems, each takes w bytes + n bytes for all '\n'
+    size_t file_size = n * n * w + n; 
+        
     int code = ftruncate(fd, file_size);
     if (code < 0) {
         goto close_file;
     }
 
-    char* buff = mmap ( NULL,               // address will be selected by the system
-                                            // (to determine the starting address of the mapping)
-                        file_size,          // size of file
-                        PROT_READ | PROT_WRITE, // read and write (able)
-                        MAP_SHARED,         // work with file, without copy
-                        fd,                 // descriptor
-                        0                   // offet
-                      );
+    char* buff = mmap(
+        NULL,      // address will be selected by the system
+                   // (to determine the starting address of the mapping)
+        file_size, // size of file
+        PROT_READ | PROT_WRITE, // read and write (able)
+        MAP_SHARED,             // work with file, without copy
+        fd,                     // descriptor
+        0                       // offet
+    );
 
-
-    int* table = (int*)malloc(n * n * sizeof(table[0]));
-    fill_table(table, n, w);
-
-    int fpos = 0;
-    for (int i = 0; i < n; ++i) {
-
-        for (int j = 0; j < n; j++) {
-
-            sprintf(buff + fpos, "%*d", w, table[i * n + j]);
-            fpos += w;
-
-        }
-
-        *(buff + fpos) = '\n';
-        ++fpos;
-
-    }
-    free(table);
+    fill_table(buff, n, w);
 
     munmap(buff, file_size);
 
