be805b9e1726183771adfa07bc6c9c4fafaea6b6
Alex Rusin <Rusin.AI@phystech.edu>
2020-11-05 19:09:02 +0300

    pupa

--- tech-07/tech07-34.c
+++ tech-07/tech07-34.c
@@ -20,10 +20,37 @@ enum desriptors {
     STDERR = 2,
 };
 
+pid_t launch(char* command, int pipe_1[2], int pipe_2[2])
+{
+    dup2(pipe_1[0], STDIN);
+    close(pipe_1[0]);
+
+    
+
+    pid_t pid = fork();
+    if (0 == pid) {
+        execlp(command, command, NULL);
+        perror("fork");
+    }
+
+    return pid;
+}
+
 int main(int argc, char* argv[])
 {
+    if (argc < 2) {
+        return UNKNOWN_ERROR;
+    }
     
-    
+    int pipes[2][2] = {{0, 0}, {0, dup(1)}};
+
+    pid_t last_pid = launch(argv[1], pipes[1], pipes[0]);
+    for (int i = 2; i < argc - 1; ++i) {
+        launch(argv[i], pipes[i & 1], pipes[1 - (i & 1)]);
+    }
+
+
+
 
     return OK;
 }
